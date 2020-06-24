564331d79e804901d71eed55e155ccd807a54688
Oliver Cook <olliewatford@gmail.com>
2020-06-24 14:31:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ var takeANumber =(line,newName) => {
   line.push(newName)
   return(`Welcome, ${newName}. You are number ${line.length} in line.`)
 }
+
+var nowServing = (line) => {
+ if (line.length !== 0) { 
+ var now = line.shift();
+ line.shift() 
+ return now
+} else { 
+return 'There is nobody waiting to be served!'
+}
+}
